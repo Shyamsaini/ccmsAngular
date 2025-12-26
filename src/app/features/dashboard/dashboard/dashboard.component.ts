64a2d23a19e1c3d0557ee1102e2cf19313c08b1f
shyam <Shyamsinghsn01@gmail.com>
2025-12-26 16:44:34 +0530

    add css and js

--- src/app/features/dashboard/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,13 +1,58 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+declare var ApexCharts: any;
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css',
-   template: `<h2>Dashboard Works!</h2>`
+  styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
 
+export class DashboardComponent implements AfterViewInit {
+ngAfterViewInit(): void {
+    this.loadCharts();
+  }
+
+  loadCharts() {
+
+    // PIE - Today
+    new ApexCharts(document.querySelector("#pieToday"), {
+      chart: { type: 'pie', height: 250 },
+      series: [80, 20],
+      labels: ['Civil', 'Criminal'],
+      colors: ['#28a745', '#007bff']
+    }).render();
+
+    // PIE - Next 7 Days
+    new ApexCharts(document.querySelector("#pieNext7"), {
+      chart: { type: 'pie', height: 250 },
+      series: [65, 20, 15],
+      labels: ['High Court', 'Supreme Court', 'District Court'],
+      colors: ['#ff851b', '#007bff', '#2ecc71']
+    }).render();
+
+    // LINE
+    new ApexCharts(document.querySelector("#lineChart"), {
+      chart: { type: 'line', height: 300 },
+      series: [{
+        name: 'Cases',
+        data: [10, 15, 30, 18, 22, 35, 28, 40, 21, 17, 25]
+      }],
+      xaxis: {
+        categories: ['Day1','Day2','Day3','Day4','Day5','Day6','Day7','Day8','Day9','Day10','Day11']
+      }
+    }).render();
+
+    // BAR
+    new ApexCharts(document.querySelector("#barChart"), {
+      chart: { type: 'bar', height: 300 },
+      series: [{
+        name: 'Cases',
+        data: [120, 340, 180, 90, 220]
+      }],
+      xaxis: {
+        categories: ['HC', 'DC', 'Sessions', 'Family', 'Special']
+      }
+    }).render();
+  }
 }
--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../shared/header/header/header.component';
+import { SidebarComponent } from '../../shared/sidebar/sidebar/sidebar.component';
+import { FooterComponent } from '../../shared/footer/footer/footer.component';
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,HeaderComponent,SidebarComponent,FooterComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css'
 })
